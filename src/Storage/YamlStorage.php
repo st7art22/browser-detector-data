733cd8091a493cec31ca7597cd724b414c12357f
Serhii Nekhaienko <serhii.nekhaienko@dev-pro.net>
2019-03-04 14:38:45 +0200

    GH-26 - Add New Robots Detection (Source: distilnetworks) - fix loader

--- src/Storage/YamlStorage.php
+++ src/Storage/YamlStorage.php
@@ -2,8 +2,8 @@
 /**
  * @author Serhii Nekhaienko <sergey.nekhaenko@gmail.com>
  * @license GPL
- * @copyright Serhii Nekhaienko &copy 2018
- * @version 4.0.0
+ * @copyright Serhii Nekhaienko &copy 2018-2019
+ * @version 4.0.2
  * @project endorphin-studio/browser-detector
  */
 
@@ -31,38 +31,9 @@ class YamlStorage extends FileStorage implements StorageInterface
             $files = $this->getFileNames();
             $this->config = [];
             foreach ($files as $file) {
-                $this->resolveData($yamlParser->parseFile($file));
+                $this->config = \array_merge_recursive($this->config, $yamlParser->parseFile($file));
             }
         }
         return $this->config;
-    }
-
-    /**
-     * Resolve data
-     * @param $data
-     * @param string $container
-     * @return array
-     */
-    public function resolveData($data, &$container = 'config'): array
-    {
-        $isRoot = false;
-        if(is_string($container)) {
-            $isRoot = true;
-            $container = $this->config;
-        }
-
-        foreach ($data as $key => $item) {
-            if(!\array_key_exists($key, $container)) {
-                $container[$key] = [];
-            }
-            if(\is_array($item)) {
-                $item = \array_merge($container[$key], $this->resolveData($item, $container[$key]));
-            }
-            $container[$key] = $item;
-        }
-        if($isRoot) {
-            $this->config = $container;
-        }
-        return $container;
     }
 }
